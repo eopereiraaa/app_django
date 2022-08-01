bd9e64b6b0cfa04941ab2d6a62ab2ee18035bf9c
eopereiraaa <pereiradev7@gmail.com>
2022-08-01 16:07:49 -0300

    some bugs fixed

--- polls/urls.py
+++ polls/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('test/', views.test, name='test'),
-
-    path('test_html', views.test_html, name='test_html')
+    path('test_html/', views.test_html, name='test_html')
 ]
